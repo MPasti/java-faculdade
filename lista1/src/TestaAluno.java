676e651c6889c23a480725c3f1e9ffd2c3f21a36
MPasti <matheusspasti@gmail.com>
2024-02-27 22:50:09 -0300

    Adicionando nova classe

---
+++ lista1/src/TestaAluno.java
@@ -0,0 +1,9 @@
+public class TestaAluno {
+    public static void main(String[] args) {
+        Aluno aluno = new Aluno(1, 18, "Matheus", 10, 8);
+
+        aluno.notaFinal();
+        aluno.dadosAluno();
+    }
+
+}
