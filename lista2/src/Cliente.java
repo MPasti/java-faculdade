5d52f5c78a95c7678176b96678feb8b956990efb
MATHEUS SOARES PASTI <24841@unifacef.edu.br>
2024-02-29 22:13:23 -0300

    trabalhadno com encapsulamento

--- lista2/src/Cliente.java
+++ lista2/src/Cliente.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class Cliente {
+public class Cliente {
+    int numeroConta, numeroAgencia;
+    String nome;
+    float saldo;
+
+    public Cliente(int numeroConta, int numeroAgencia, String nome, float saldo) {
+        this.numeroConta = numeroConta;
+        this.numeroAgencia = numeroAgencia;
+        this.nome = nome;
+        this.saldo = saldo;
+    }
+
+    public void realizarDeposito(){
+
+    }
+
+    public void realizarSaque(){
+
+    }
+
+    public void dadosCliente(){
+        System.out.print("Numero da Conta: " + this.numeroConta
+                + "\nNumero da agência: " + this.numeroConta
+                + "\nSaldo: " + this.saldo);
+    }
+
 }
