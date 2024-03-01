2b52411af0b34cfe71a20fddfd880ecc3e941ff1
MATHEUS SOARES PASTI <24841@unifacef.edu.br>
2024-02-29 22:11:58 -0300

    Trabalhando com encapsulamento

---
+++ lista2b/src/main/Main.java
@@ -0,0 +1,12 @@
+package main;
+import produto.Produto;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Produto produto = new Produto();
+
+        produto.setCodigo(3);
+
+    }
+}
