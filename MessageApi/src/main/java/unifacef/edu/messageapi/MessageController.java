5c3017998c0d86b768e4d419052dba13884f549e
MATHEUS SOARES PASTI <24841@unifacef.edu.br>
2024-05-02 22:08:44 -0300

    novas rotas e métodos

--- MessageApi/src/main/java/unifacef/edu/messageapi/MessageController.java
+++ MessageApi/src/main/java/unifacef/edu/messageapi/MessageController.java
@@ -1,11 +1,15 @@
 package unifacef.edu.messageapi;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.apache.catalina.User;
+import org.springframework.web.bind.annotation.*;
 
 @RestController //essa classe controla as requisições
 public class MessageController {
 
+    @GetMapping("/")
+    public String home(){
+        return "<h1>HAHAHAHHAHAHA</h1>";
+    }
     @GetMapping("/dia")
     public String bomDia(){
         return "bom dia";
@@ -17,5 +21,25 @@ public class MessageController {
     @GetMapping("/noite")
     public String boaNoite(){
         return "boa noite zé";
+    }
+
+    @PostMapping("/user")
+    public String user(@RequestBody User user){
+        return "usuário criado!" + user;
+    }
+
+    @DeleteMapping("/user")
+    public String userDelete(){
+        return "usuário deletado!";
+    }
+
+    @PutMapping("/user")
+    public String userUpdate(){
+        return "usuário atualizado!";
+    }
+
+    @PatchMapping("/user/{id}")
+    public String idUpdate(@PathVariable int id){
+        return "id atualizado! " + id;
     }
 }
